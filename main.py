5fd1bca495570c8d6789492a5c33edf2ec769319
banana <birdus.nazarii@gmail.com>
2023-11-07 01:14:06 +0000

    added heatmap prototype

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template
+import random
 
+from flask import Flask, render_template
+from datetime import datetime, timedelta
 import trash
 
 app = Flask(__name__)
-
+my_history = []
+my_history_formated = []
 
 @app.route('/')
 def index():
     trash.Screenshot()
-    return render_template('index.html')
-
+    return render_template('cheatmap.html' , table_data=my_history_formated)
 
 
 
@@ -17,4 +19,15 @@ def index():
 
 
 if __name__ == '__main__':
+    for i in range(0,366):
+        my_history.append([(datetime.today() - timedelta(days=i)).strftime("%x"), random.randint(0,4)])
+
+    for rows in range(6):
+        my_history_formated.append([])
+        for cols in range(61):
+            my_history_formated[rows].append(my_history.pop(0))
+
+
+
+
     app.run()
